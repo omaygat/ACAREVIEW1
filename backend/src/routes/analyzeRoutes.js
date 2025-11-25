32265f0ba644185768928d63469ca5b28c42e6b3
ACAREVIEW <mallmita720@gmail.com>
2025-11-25 16:20:26 -0500

    Fix: actualizar

--- backend/src/routes/analyzeRoutes.js
+++ backend/src/routes/analyzeRoutes.js
@@ -4,7 +4,7 @@ import { analyzeText } from "../controllers/analiza_control.js";
 import { uploadFileHandler } from "../controllers/fileController.js";
 
 const router = express.Router();
-
+router.post("/send-n8n", sendToN8N);
 // Guardar archivos temporalmente en carpeta /uploads
 const upload = multer({ dest: "uploads/" });
 
